257e27fffb50bfa7a338ffd84a0115707e9b756f
StanceFreak <jombor.army67@gmail.com>
2023-01-05 01:13:45 +0800

    Added api call and updating the dependecies

--- app/src/main/java/com/example/testing/views/adapter/MedicineListAdapter.kt
+++ app/src/main/java/com/example/testing/views/adapter/MedicineListAdapter.kt
@@ -3,11 +3,41 @@ package com.example.testing.views.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.testing.R
 import com.example.testing.databinding.RvHomeBinding
+import com.example.testing.data.api.model.Result
+import com.squareup.picasso.Picasso
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.min
 
 class MedicineListAdapter : RecyclerView.Adapter<MedicineListAdapter.RecyclerviewHolder>() {
-    inner class RecyclerviewHolder (private val binding: RvHomeBinding) : RecyclerView.ViewHolder(binding.root){
 
+    private var data = ArrayList<Result>()
+
+    inner class RecyclerviewHolder (private val binding: RvHomeBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(result: Result) {
+            val localeID =  Locale("in", "ID")
+            val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+            Picasso.get()
+                .load(result.thumbnailUrl)
+                .placeholder(R.drawable.ic_image_placeholder)
+                .fit()
+                .into(binding.productImg)
+            binding.productName.text = result.name
+            binding.productPrice.text = numberFormat.format(result.minPrice)
+                .replace("Rp", "Rp ")
+                .replace(",00", "")
+//            binding.productPrice.text = "%,d".format(result.minPrice)
+            if (result.visualCues.contains("in_stock")) {
+                binding.productStatus.text = "Tersedia"
+            }
+            else {
+                binding.productStatus.text = "Stok Habis"
+            }
+            binding.productSellUnit.text = result.sellingUnit.replace("Per ", "/")
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerviewHolder {
@@ -20,10 +50,17 @@ class MedicineListAdapter : RecyclerView.Adapter<MedicineListAdapter.Recyclervie
     }
 
     override fun onBindViewHolder(holder: RecyclerviewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val data = this.data[position]
+        holder.bind(data)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return min(this.data.size, 8)
+    }
+
+    fun setData(datalist: List<Result>) {
+        this.data.clear()
+        this.data.addAll(datalist)
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/testing/views/home/HomeActivity.kt
+++ app/src/main/java/com/example/testing/views/home/HomeActivity.kt
@@ -5,25 +5,70 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.testing.R
 import com.example.testing.data.api.factory.ApiViewModelFactory
+import com.example.testing.data.api.network.Api
 import com.example.testing.data.api.network.ApiClient
 import com.example.testing.data.api.network.ApiHelper
 import com.example.testing.databinding.ActivityHomeBinding
 import com.example.testing.util.Status
+import com.example.testing.views.adapter.MedicineListAdapter
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var viewModel: HomeViewModel
+    private lateinit var medicineListAdapter: MedicineListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
-
+        setupAdapter()
+        setupApiCall()
         setContentView(binding.root)
     }
 
+    private fun setupAdapter() {
+        medicineListAdapter = MedicineListAdapter()
+        binding.rvHome.apply {
+            layoutManager = GridLayoutManager(
+                this@HomeActivity,
+                2,
+            )
+            adapter = medicineListAdapter
+        }
+    }
 
+    private fun setupApiCall() {
+
+        viewModel = ViewModelProvider(
+            this,
+            ApiViewModelFactory(ApiHelper(ApiClient.instance))
+        ).get(HomeViewModel::class.java)
+
+        viewModel.getPenawaranSpecialMedicine().observe(this) {
+            it?.let { resource ->
+                when(resource.status) {
+                    Status.SUCCESS -> {
+                        binding.shimmerPlaceholderContainer.visibility = View.GONE
+                        binding.rvHome.visibility = View.VISIBLE
+                        resource.data?.let { response ->
+                            medicineListAdapter.setData(response.result)
+                        }
+                    }
+                    Status.ERROR -> {
+                        binding.shimmerPlaceholderContainer.stopShimmer()
+                        binding.shimmerPlaceholderContainer.visibility = View.INVISIBLE
+                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    }
+                    Status.LOADING -> {
+                        binding.shimmerPlaceholderContainer.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+
+    }
 }

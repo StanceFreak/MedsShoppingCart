d2f202c276f6bac8d365ace3b136dfbb3354923a
StanceFreak <jombor.army67@gmail.com>
2023-01-05 01:14:29 +0800

    Updating the api base url

--- app/src/main/java/com/example/testing/data/api/network/ApiClient.kt
+++ app/src/main/java/com/example/testing/data/api/network/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://127.0.0.1:3000"
+    private const val BASE_URL = "http://10.0.2.2:3000"
 
     val instance: Api by lazy {
 

4cfd56dda4ed375847c586260999e3999c85cc3f
StanceFreak <jombor.army67@gmail.com>
2023-01-06 23:57:36 +0800

    Added api call based by item id

--- app/src/main/java/com/example/testing/views/detail/ItemDetailActivity.kt
+++ app/src/main/java/com/example/testing/views/detail/ItemDetailActivity.kt
@@ -2,17 +2,35 @@ package com.example.testing.views.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.testing.R
+import com.example.testing.data.api.factory.ApiViewModelFactory
+import com.example.testing.data.api.network.Api
+import com.example.testing.data.api.network.ApiClient
+import com.example.testing.data.api.network.ApiHelper
 import com.example.testing.databinding.ActivityItemDetailBinding
+import com.example.testing.util.Status
+import com.squareup.picasso.Picasso
+import java.text.NumberFormat
+import java.util.*
 
 class ItemDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityItemDetailBinding
+    private lateinit var viewModel: ItemDetailViewModel
+
+    companion object {
+        const val ITEM_SLUG ="item_slug"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityItemDetailBinding.inflate(layoutInflater)
         setupToolbar()
+        setupApiCall()
         setContentView(binding.root)
     }
 
@@ -26,5 +44,73 @@ class ItemDetailActivity : AppCompatActivity() {
         binding.itemDetailToolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    private fun setupApiCall() {
+
+        val slug = intent.getStringExtra(ITEM_SLUG)!!
+
+        viewModel = ViewModelProvider(
+            this,
+            ApiViewModelFactory(ApiHelper(ApiClient.instance))
+        ).get(ItemDetailViewModel::class.java)
+
+        viewModel.getMedicineById(slug).observe(this) {
+            it?.let { resource ->
+                when(resource.status) {
+                    Status.SUCCESS -> {
+                        binding.shimmerPlaceholderContainer.visibility = View.GONE
+                        binding.itemDetailContent.visibility = View.VISIBLE
+                        resource.data?.let { response ->
+                            val localeID =  Locale("in", "ID")
+                            val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+                            Picasso.get()
+                                .load(response.thumbnailUrl)
+                                .placeholder(R.drawable.ic_image_placeholder)
+                                .into(binding.itemDetailThumbnail)
+                            binding.apply {
+                                if (response.name.contains(" - ")) {
+                                    itemDetailTitle.text = response.name
+                                        .substring(0, response.name.indexOf(" - "))
+                                    itemDetailName.text = response.name
+                                        .substring(0, response.name.indexOf(" - "))
+                                }
+                                else {
+                                    itemDetailTitle.text = response.name
+                                    itemDetailName.text = response.name
+                                }
+
+                                itemDetailPrice.text = numberFormat.format(response.minPrice)
+                                    .replace("Rp", "Rp ")
+                                    .replace(",00", "")
+                                itemDetailSellingUnit.text = response.sellingUnit
+                                itemDetailCategory.text = response.categories[0].name
+                                itemDetailDesc.text = response.description
+                                itemDetailGeneralIndication.text = response.generalIndication
+                                itemDetailComposition.text = response.composition
+                                itemDetailDosage.text = response.dosage
+                                itemDetailHowToUse.text = response.howToUse
+                                itemDetailWarning.text = response.warning
+                                itemDetailSideEffects.text = response.sideEffects
+                                itemDetailManufacturer.text = response.manufacturerName
+                                itemDetailBpomNumber.text = response.bpomNumber
+                            }
+
+                        }
+                    }
+                    Status.ERROR -> {
+                        binding.shimmerPlaceholderContainer.stopShimmer()
+                        binding.shimmerPlaceholderContainer.visibility = View.INVISIBLE
+                        binding.itemDetailContent.visibility = View.GONE
+                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    }
+                    Status.LOADING -> {
+                        binding.shimmerPlaceholderContainer.visibility = View.VISIBLE
+                        binding.itemDetailContent.visibility = View.GONE
+                    }
+                }
+            }
+        }
+
     }
 }

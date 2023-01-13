3c2391fc437222db964c3e3528c658b238ed8aec
StanceFreak <jombor.army67@gmail.com>
2023-01-13 19:02:57 +0800

    Refactor the model code to prevent no-argument constructor error

--- app/src/main/java/com/example/testing/data/api/model/shoppingCart/CartList.kt
+++ app/src/main/java/com/example/testing/data/api/model/shoppingCart/CartList.kt
@@ -1,9 +1,9 @@
 package com.example.testing.data.api.model.shoppingCart
 
 data class CartList(
-    val slug: String,
-    val name: String,
-    val thumbnailUrl: String,
-    val minPrice: Int,
-    val quantity: Int
+    val slug: String? = "",
+    val name: String? = "",
+    val thumbnailUrl: String? = "",
+    val minPrice: Int? = 0,
+    val quantity: Int? = 0
 )

7a6890224161225f7a5261de170c6d9a505d02c0
StanceFreak <jombor.army67@gmail.com>
2023-01-13 23:23:00 +0800

    Added checked & unchecked all checkboxes and display total price of all rv item

--- app/src/main/java/com/example/testing/views/adapter/ShoppingCartAdapter.kt
+++ app/src/main/java/com/example/testing/views/adapter/ShoppingCartAdapter.kt
@@ -3,6 +3,8 @@ package com.example.testing.views.adapter
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testing.R
 import com.example.testing.data.api.model.shoppingCart.CartList
@@ -11,11 +13,11 @@ import com.example.testing.views.detail.ItemDetailActivity
 import com.squareup.picasso.Picasso
 import java.text.NumberFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 class ShoppingCartAdapter: RecyclerView.Adapter<ShoppingCartAdapter.RecyclerviewHolder>() {
 
     private var data = ArrayList<CartList>()
+    private var isCheckedAll = false
 
     inner class RecyclerviewHolder(private val binding: RvCartBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(cartList: CartList) {
@@ -27,7 +29,7 @@ class ShoppingCartAdapter: RecyclerView.Adapter<ShoppingCartAdapter.Recyclerview
                     .placeholder(R.drawable.ic_image_placeholder)
                     .fit()
                     .into(itemCartThumbnail)
-                if (cartList.name.contains(" - ")) {
+                if (cartList.name?.contains(" - ") == true) {
                     itemCartName.text = cartList.name
                         .substring(0, cartList.name.indexOf("-"))
                 }
@@ -37,7 +39,8 @@ class ShoppingCartAdapter: RecyclerView.Adapter<ShoppingCartAdapter.Recyclerview
                 itemCartPrice.text = numberFormat.format(cartList.minPrice)
                     .replace("Rp", "Rp ")
                     .replace(",00", "")
-                itemCartQuantity.setText(cartList.quantity)
+                itemCartQuantity.setText(cartList.quantity.toString())
+                itemCartCb.isChecked = isCheckedAll
             }
         }
     }
@@ -54,6 +57,14 @@ class ShoppingCartAdapter: RecyclerView.Adapter<ShoppingCartAdapter.Recyclerview
     override fun onBindViewHolder(holder: RecyclerviewHolder, position: Int) {
         val data = this.data[position]
         holder.bind(data)
+        val btnReduce = holder.itemView.findViewById<ImageView>(R.id.item_cart_reduce)
+        btnReduce.setOnClickListener {
+            Toast.makeText(
+                holder.itemView.context,
+                "Berhasil menambahkan ke keranjang!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 
         holder.itemView.setOnClickListener{
             val i = Intent(holder.itemView.context, ItemDetailActivity::class.java)
@@ -64,6 +75,25 @@ class ShoppingCartAdapter: RecyclerView.Adapter<ShoppingCartAdapter.Recyclerview
 
     override fun getItemCount(): Int {
         return this.data.size
+    }
+
+    fun getTotalPrice(dataList: List<CartList>) : Int {
+        var totalPrice = 0
+        for (item in dataList) {
+            totalPrice += item.minPrice!!
+
+        }
+        return totalPrice
+    }
+
+    fun cbSelectAll() {
+        isCheckedAll = true
+        notifyDataSetChanged()
+    }
+
+    fun cbUnselectAll() {
+        isCheckedAll = false
+        notifyDataSetChanged()
     }
 
     fun setData(dataList: List<CartList>) {

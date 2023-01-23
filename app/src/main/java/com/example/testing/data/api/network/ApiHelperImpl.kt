cf5dceb97f7ac9e436a81ed3796489574f3cf384
StanceFreak <jombor.army67@gmail.com>
2023-01-23 20:54:29 +0800

    Added Impl file for the ApiHelper class

---
+++ app/src/main/java/com/example/testing/data/api/network/ApiHelperImpl.kt
@@ -0,0 +1,20 @@
+package com.example.testing.data.api.network
+
+import com.example.testing.data.api.model.medicine.MedicineDetail
+import com.example.testing.data.api.model.medicine.MedicineListModel
+import retrofit2.Response
+import javax.inject.Inject
+
+class ApiHelperImpl @Inject constructor(
+    private val apiService: ApiService
+): ApiHelper {
+
+    override suspend fun getPenawaranSpecialMedicine(): Response<MedicineListModel> {
+        return apiService.getPenawaranSpecialMedicine()
+    }
+
+    override suspend fun getMedicineById(slug: String): Response<MedicineDetail> {
+        return apiService.getMedicineById(slug)
+    }
+
+}

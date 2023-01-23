17c4f803d041a7d7386ec81f97ff4ba17a088269
StanceFreak <jombor.army67@gmail.com>
2023-01-23 20:55:17 +0800

    Adding @Inject annotation and change the return type of each function

--- app/src/main/java/com/example/testing/data/api/repository/MedicineRepository.kt
+++ app/src/main/java/com/example/testing/data/api/repository/MedicineRepository.kt
@@ -3,15 +3,16 @@ package com.example.testing.data.api.repository
 import com.example.testing.data.api.model.medicine.MedicineDetail
 import com.example.testing.data.api.model.medicine.MedicineListModel
 import com.example.testing.data.api.network.ApiHelper
+import retrofit2.Response
+import javax.inject.Inject
 
-class MedicineRepository(private val apiHelper: ApiHelper) {
+class MedicineRepository @Inject constructor(private val apiHelper: ApiHelper) {
 
-    suspend fun getPenawaranSpecialMedicine() : MedicineListModel {
+    suspend fun getPenawaranSpecialMedicine() : Response<MedicineListModel> {
         return apiHelper.getPenawaranSpecialMedicine()
     }
 
-
-    suspend fun getMedicineById(slug: String) : MedicineDetail {
+    suspend fun getMedicineById(slug: String) : Response<MedicineDetail> {
         return apiHelper.getMedicineById(slug)
     }
 

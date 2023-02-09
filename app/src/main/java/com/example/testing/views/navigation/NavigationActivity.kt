a7683711ce05900e6bd3e5edd4687150473642bc
StanceFreak <jombor.army67@gmail.com>
2023-02-09 18:21:11 +0800

    Added navController to the navigation and hiding the bottom navigation on specific a fragment

--- app/src/main/java/com/example/testing/views/navigation/NavigationActivity.kt
+++ app/src/main/java/com/example/testing/views/navigation/NavigationActivity.kt
@@ -2,10 +2,16 @@ package com.example.testing.views.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.testing.R
 import com.example.testing.databinding.ActivityNavigationBinding
+import com.example.testing.views.cart.ShoppingCartFragment
+import com.example.testing.views.detail.ItemDetailFragment
 import com.example.testing.views.home.HomeFragment
 
 class NavigationActivity : AppCompatActivity() {
@@ -15,8 +21,22 @@ class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNavigationBinding.inflate(layoutInflater)
+        setupNavController()
         setupBottomNav()
         setContentView(binding.root)
+    }
+
+    private fun setupNavController() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHost.navController
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment
+            )
+        )
+        navController.addOnDestinationChangedListener { _: NavController, destination: NavDestination, _: Bundle? ->
+            binding.navBottomNav.isVisible = appBarConfig.topLevelDestinations.contains(destination.id)
+        }
     }
 
     private fun setupBottomNav() {
